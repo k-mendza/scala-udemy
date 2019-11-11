83e849a95f483b70f967fe6b2434bed23bb6e294
k-mendza <karolmendza@gmail.com>
2019-11-11 21:23:43 +0100

    Method exercises

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -2,13 +2,17 @@ package lectures.part2oop
 
 object MethodNotations extends App {
 
-  class Person(val name: String, favoriteMovie: String) {
+  class Person(val name: String, favoriteMovie: String, val age: Int = 0) {
     def likes(movie: String): Boolean = movie == favoriteMovie
     def hangOutWith(person: Person): String = s"${this.name} is hanging out with ${person.name}"
     def unary_! : String = s"what the heck?!"
+    def unary_+ : Person = new Person(name, favoriteMovie, age + 1)
+    def +(nickName: String) : Person = new Person(s"$name ($nickName)", favoriteMovie)
+    def learns(subject: String) : String = s"$name learns $subject"
+    def learnScala() : String = learns("Scala")
     def isAlive : Boolean = true
-
     def apply(): String = s"Hi, my name is $name and i like $favoriteMovie"
+    def apply(n: Int) : String = s"$name watched $favoriteMovie $n times"
   }
 
   val mary = new Person("Mary", "Inception")
