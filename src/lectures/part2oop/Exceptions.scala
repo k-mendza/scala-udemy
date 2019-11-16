2ed32aa027c964a223475f48235a56c246356284
Karol Mendza <karolmendza@gmail.com>
2019-11-16 11:44:34 +0100

    Exceptions

--- src/lectures/part2oop/Exceptions.scala
+++ src/lectures/part2oop/Exceptions.scala
@@ -1,5 +1,7 @@
 package lectures.part2oop
 
+import java.nio.{BufferOverflowException, BufferUnderflowException}
+
 object Exceptions extends App {
 
   def isExceptionThrown(throwException: Boolean): Int = {
@@ -16,4 +18,41 @@ object Exceptions extends App {
   }
 
   println(myIntVal)
+
+  class MyException extends Exception
+  val exception = new MyException
+//  throw exception
+
+  object PocketCalculator {
+    def add(x: Int, y: Int): Int = {
+      val result = x + y
+      if ( x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if (x < 0 && y < 0 && result > 0) throw new UnderflowException
+      else result
+    }
+    def subtract(x: Int, y: Int): Int = {
+      val result = x - y
+      if ( x > 0 && y < 0 && result < 0) throw new OverflowException
+      else if ( x < 0 && y > 0 && result > 0) throw new UnderflowException
+      else result
+    }
+    def multiply(x: Int, y: Int): Int = {
+      val result = x * y
+      if ( x > 0 && y > 0 && result < 0) throw new OverflowException
+      else if ( x < 0 && y < 0 && result < 0) throw new OverflowException
+      else if ( x > 0 && y < 0 && result > 0) throw new UnderflowException
+      else if ( x < 0 && y > 0 && result > 0) throw new UnderflowException
+      else result
+    }
+    def divide(x: Int, y: Int): Int = {
+      if (x == 0 | y == 0) throw new MathCalculationException
+      else x / y
+    }
+    class OverflowException extends Exception
+    class UnderflowException extends Exception
+    class MathCalculationException extends Exception
+  }
+
+//  println(PocketCalculator.add(Int.MaxValue, 10))
+
 }
